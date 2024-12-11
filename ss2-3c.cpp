14c2d6c674b450bf69c99c26c60e168f20f334df
minhnhat1610 <161536740+minhnhat1610@users.noreply.github.com>
2024-12-11 07:50:15 +0700

    Add files via upload

---
+++ ss2-3c.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  
+    int num1 = 10; 
+    int num2 = 20; 
+
+    int sum = num1 + num2;       
+    int difference = num1 - num2;
+    int product = num1 * num2;   
+    float quotient = (float)num1 / num2;             
+
+    printf("Tổng của %d và %d là: %d\n", num1, num2, sum);
+    printf("Hiệu của %d và %d là: %d\n", num1, num2, difference);
+    printf("Tích của %d và %d là: %d\n", num1, num2, product);
+    printf("Thương của %d và %d là: %.2f\n", num1, num2, quotient);
+
+    return 0;
+}
